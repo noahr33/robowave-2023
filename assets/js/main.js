5e576a88d8650e50d77f9dd855ddb1760f50784e
Josh <zylo.codes@gmail.com>
2023-06-27 20:58:26 -0500

    fullpage and started typewriter effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+  // fullpage js
+  $('#fullpage').fullpage({
+    licenseKey: 'gplv3-license',
+    navigation: true,
+    navigationTooltips: ['Welcome', 'New Robots', 'Used Robots', 'Contact'] 
+  });
+
+  // typewriterjs
+  var typewriterElements = document.querySelectorAll('.typewriter-effect')
+  for (var i = 0; i < typewriterElements.length; i++) {
+    var currentEl = typewriterElements[i]
+  }
+
+});
