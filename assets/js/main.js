437480aa7f2f0aac9377b405a711c0a992143a3f
Noah <noahkoleske@icloud.com>
2023-07-10 18:17:49 -0500

    merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: 'gplv3-license',
+        navigation: true,
+        navigationTooltips: ['welcome', 'New Robots', 'Used Robots', 'Plugin']
+    });
+
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+    for (var i = 0; i < typewriterElements.length; i++) {
+        var currentEl = typewriterElements[i]
+        var innerText = currentEl.innerText
+        
+        new Typewriter(currentEl, {
+            loop: true,
+    
+        })
+        .typeString(innerText)
+        .pauseFor(1000)
+        .start()
+    }
+
+    PowerGlitch.glitch('.glitch')
+    hideOverflow: true
+});
